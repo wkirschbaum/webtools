2a7e8b4dfdb42416b97866349b7b46c9d0783dbf
Wilhelm Kirschbaum <wkirschbaum@gmail.com>
2015-05-28 11:32:33 +0200

    prepare for dokku

--- main.go
+++ main.go
@@ -4,10 +4,11 @@ import (
 	"fmt"
 	"html"
 	"net/http"
+	"os"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("<html><form action='/convert'><textarea type='text' name='whatver'></textarea><button>click me</button></form></html>"))
+	w.Write([]byte("<html><form method='POST' action='/convert'><textarea type='text' name='whatver'></textarea><button>click me</button></form></html>"))
 }
 
 func convert(w http.ResponseWriter, r *http.Request) {
@@ -20,5 +21,19 @@ func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/convert", convert)
-	http.ListenAndServe(":3001", mux)
+	err := http.ListenAndServe(":"+getListenPort(), mux)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func getListenPort() (port string) {
+	port = os.Getenv("PORT")
+
+	if len(port) == 0 {
+		port = "3000"
+	}
+
+	fmt.Printf("Listening on port %s \n", port)
+	return
 }
